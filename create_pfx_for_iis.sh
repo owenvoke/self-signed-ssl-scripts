cc7c3ac5ead41bbd0dd3a05bb1785fabae46e5b3
PXgamer <owzie123@gmail.com>
2017-07-26 10:24:53 +0100

    Added scripts

---
+++ create_pfx_for_iis.sh
@@ -0,0 +1,9 @@
+if [ -z "$1" ]
+then
+  echo "Please supply a subdomain to create a certificate for...";
+  echo "e.g. dev.local"
+  exit;
+fi
+
+DOMAIN=$1
+openssl pkcs12 -export -out $DOMAIN/$DOMAIN.pfx -inkey $DOMAIN/device.key -in $DOMAIN/$DOMAIN.crt
